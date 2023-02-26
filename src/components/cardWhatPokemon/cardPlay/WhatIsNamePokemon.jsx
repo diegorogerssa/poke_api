dc67a6043fb4b5dbaaf0abc23736f4761b91407b
Diego <rogersmhdg@gmail.com>
2023-02-26 20:27:36 -0300

    atualizar botoes do alphabet quando a pokebola é usada

--- src/components/cardWhatPokemon/cardPlay/WhatIsNamePokemon.jsx
+++ src/components/cardWhatPokemon/cardPlay/WhatIsNamePokemon.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useContext, useState, useRef } from 'react';
 import DataContext from '../../../contexts/DataContext';
 import LoserPopup from '../LoserPopup';
-
 import StyledWapper from './styles/StyledWapper';
 import StyledNamePokemon from './styles/StyledNamePokemon';
 import StyledPointsLife from './styles/StyledPointsLife';
@@ -10,37 +9,47 @@ import StyledAlfa from './styles/StyledAlfa';
 import StyledHeading from '../../shared/styled/StyledHeading';
 import StyledPokebola from './styles/StyledPokebola';
 import StyledSpanPokeball from './styles/StyledSpanPokeball';
-
 import poke from '../../../assets/pokebola-loser.png';
 
 const CardWhatIsNamePokemon =  () => {
-  const { pokemon, winner, setWinner, loser, setLoser } = useContext(DataContext);
+  const { 
+    pokemon, 
+    winner, 
+    setWinner, 
+    loser, 
+    setLoser 
+  } = useContext(DataContext);
 
   const [array, setArray] = useState([]);
   const [pointsLife, setPointsLife] = useState(3);
-  // const [pokeballs, setPokeballs] = useState(5);
-  let pokeballRef = useRef(6);
+  let pokeballRef = useRef(5); //@TODO: Refatorar para usar o useState
+  const alphabetRefs = useRef([]);
 
-  const nomeArray =pokemon.name
-    ? pokemon.name.split('')
+  const nameArray = pokemon.name 
+    ? pokemon.name.split('') 
     : []; 
 
-  const alfabeto = 'abcdefghijklmnopqrstuvwxyz'.toLowerCase().split('');
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    .toLowerCase()
+    .split('');
     
-  const arrayFill = new Array(nomeArray.length).fill(' ');
-  useEffect(() => {
-    setArray(arrayFill);
+  const arrayFill = new Array(nameArray.length).fill(' ');
   
-  }, [pokemon]);
-
   const usePokeball = () => {
     if (pokeballRef.current > 0) {
       pokeballRef.current = pokeballRef.current - 1;
       const index = array.findIndex((letra) => letra === ' '); 
-      const letraIndex = nomeArray[index];
-      nomeArray.filter((letra, i) => {
+      const letraIndex = nameArray[index];
+      nameArray.filter((letra, i) => {
         if(letra === letraIndex) {
           arrayFill[i] = letraIndex;
+          alphabet.filter((letra, i) => {
+            if(letra === letraIndex) {
+              alphabetRefs.current[i].style.color = '#02e202';
+              alphabetRefs.current[i].disabled = true;
+              alphabetRefs.current[i].style.cursor = 'not-allowed';
+            }
+          });
           setArray(prevArray => {
             const newArray = [...prevArray];
             newArray[i] = letraIndex;
@@ -48,38 +57,13 @@ const CardWhatIsNamePokemon =  () => {
           },);
         }
       });
-      console.log('usePokebals', pokeballRef.current);
     }
   };
-  useEffect(() => {
-    localStorage.setItem('pokeballs', pokeballRef.current);
-    console.log('useEffects', pokeballRef.current);
-  }, [pokeballRef.current]);
 
-  const stringArray = array.join('');
-  const namePokemon = pokemon.name;
-  const verifyWinner = () => {
-    if( stringArray === namePokemon) {
-      setWinner(true);
-      pokeballRef.current = pokeballRef.current + 1;
-    }
-  };
-  useEffect(() => {
-    verifyWinner();
-  }, [array]);
 
-  useEffect(() => {
-    if (pointsLife < 1 ){
-      setLoser(true);
-    }
-  }, [pointsLife]);
-  
-
-  
-
-  const captureLetra = (e) => {
+  const captureLetter = (e) => {
     const letraCapturada = e.target.innerText;
-    nomeArray.filter((letra, i) => {
+    nameArray.filter((letra, i) => {
       if(letra === letraCapturada) {
         arrayFill[i] = letraCapturada;
         setArray(prevArray => {
@@ -90,9 +74,7 @@ const CardWhatIsNamePokemon =  () => {
           e.target.style.cursor = 'not-allowed';
           return newArray;
         },);
-      
-      } else if (!nomeArray.includes(letraCapturada)) {
-  
+      } else if (!nameArray.includes(letraCapturada)) {
         e.target.style.color = '#b40606';
         e.target.disabled = true;
         e.target.style.cursor = 'not-allowed';
@@ -101,7 +83,33 @@ const CardWhatIsNamePokemon =  () => {
     });
   };
 
-  //#bfbfbf
+  const verifyWinner = () => {
+    const stringArray = array.join('');
+    const namePokemon = pokemon.name;
+    if( stringArray === namePokemon) {
+      setWinner(true);
+      pokeballRef.current = pokeballRef.current + 1;
+    }
+  };
+
+  useEffect(() => {
+    setArray(arrayFill);
+  }, [pokemon]);
+
+  useEffect(() => {
+    localStorage.setItem('pokeballs', pokeballRef.current);
+  }, [pokeballRef.current]);
+
+  useEffect(() => {
+    verifyWinner();
+  }, [array]);
+
+  useEffect(() => {
+    if (pointsLife < 1 ){
+      setLoser(true);
+    }
+  }, [pointsLife]);
+  
   return (
     loser ? <LoserPopup /> :
       <StyledWapper
@@ -134,7 +142,6 @@ const CardWhatIsNamePokemon =  () => {
           height = '20vh'
           width =  '50vw'
           backgroundColor = {({theme}) => theme.light.palette.secondary.yellow}
-          // border = '1px solid #000'
         >
           <StyledPointsLife>{pointsLife}</StyledPointsLife>
           <StyledGerericWrapper
@@ -144,14 +151,12 @@ const CardWhatIsNamePokemon =  () => {
             backgroundColor = {({theme}) => theme.light.palette.secondary.yellow}
             onClick = {usePokeball}
           >
-            
             <StyledPokebola src={poke} />   
             <StyledSpanPokeball>{ pokeballRef.current }</StyledSpanPokeball>
             
           </StyledGerericWrapper>      
         </StyledGerericWrapper>
         } 
-
         {
           winner &&
             <StyledGerericWrapper
@@ -160,7 +165,6 @@ const CardWhatIsNamePokemon =  () => {
               height = '20vh'
               width =  '50vw'
               backgroundColor = {({theme}) => theme.light.palette.secondary.yellow}
-              // border = '1px solid #000'
             >
               <StyledHeading
                 level={3}
@@ -173,16 +177,17 @@ const CardWhatIsNamePokemon =  () => {
                 backgroundColor = {({theme}) => theme.light.palette.secondary.yellow}
                 onClick = {usePokeball}
               >
-                
                 <StyledPokebola src={poke} />   
                 
               </StyledGerericWrapper>      
             </StyledGerericWrapper>
         }  
-
         <StyledGerericWrapper>
           { 
-            alfabeto.map((letra, i) => <StyledAlfa key={i} onClick = {captureLetra}>{letra}</StyledAlfa>)
+            alphabet.map((letra, i) => <StyledAlfa 
+              key={i} 
+              ref={(element) => alphabetRefs.current[i] = element}
+              onClick = {captureLetter}>{letra}</StyledAlfa>)
           }
         </StyledGerericWrapper>
       </StyledWapper>
